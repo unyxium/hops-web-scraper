644f4b04dd568470a781b5bf5e389c24e7955079
unyxium <unyxium@gmail.com>
2022-06-22 21:29:38 +1000

    functional scraper

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from rhino3dm import *
-import time
 import ghhops_server as hs
+import time
+import requests
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 hops = hs.Hops(app)
-
-print(time.time())
 
 
 @hops.component(
@@ -16,43 +17,65 @@ print(time.time())
     icon="icons/star.png",
     inputs=[],
     outputs=[
-        hs.HopsInteger("Time", "T", "Current time for version tracking")
+        hs.HopsInteger("Time", "T", "Current time for version tracking"),
+        hs.HopsString("Author", "A", "Author")
     ]
 )
 def info():
-    return round(time.time())
-
-
-'''
-@hops.component(
-    "/pointat",
-    name="PointAt",
-    description="Get point along curve",
-    icon="icons/star.png",
-    inputs=[
-        hs.HopsCurve("Curve", "C", "Curve to evaluate"),
-        hs.HopsNumber("t", "t", "Parameter on Curve to evaluate"),
-    ],
-    outputs=[
-        hs.HopsPoint("P", "P", "Point on curve at t")
-    ]
-)
-def pointat(curve, t):
-    return curve.PointAt(t)
-'''
+    return round(time.time()), 'unyxium'
 
 
 @hops.component(
-    "/upper",
-    name="uppercase",
-    description="Get uppercase string",
+    "/scraper",
+    name="Web Scraper",
+    description="Scrape a webpage for data",
     icon="icons/star.png",
     inputs=[
-        hs.HopsString("String", "S", "Input string"),
+        hs.HopsString("URL", "U", "Webpage URL"),
+        hs.HopsString("Elements", "E", "List of targeted elements in order",
+                      access="HopsParamAccess.TREE")
     ],
     outputs=[
-        hs.HopsString("Output", "O", "Uppercase string")
+        hs.HopsString("Output", "O", "Output data")
     ]
 )
-def makeupper(S):
-    return S.upper()
+def scrape(location, tree):
+    #location = 'https://example.com'
+    #tree = ['p']
+
+    page = requests.get(location)
+
+    if page.status_code == 200:
+        subpage = [BeautifulSoup(page.content, features='lxml')]
+
+        for tag in tree:
+            for item in subpage:
+                # found objs is for the next iteration
+                found_objs = []
+                for obj in item.find_all(tag):
+                    found_objs.append(obj)
+                #print('FOUND OBJS ::::: ', found_objs)
+            subpage = found_objs
+
+        # this took ages to debug... turns out the items weren't strings
+        result = [str(item) for item in subpage]
+
+        return result
+    else:
+        return [f'Could not connect to {location}']
+
+
+@hops.component(
+    "/test",
+    name="TEST",
+    description="Testing",
+    icon="icons/star.png",
+    inputs=[
+        hs.HopsString("Input", "I", "Input values")
+    ],
+    outputs=[
+        hs.HopsString("Output", "O", "Output values")
+    ]
+)
+def testing(i):
+    return i
