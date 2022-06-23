9bd62570a6bd5ae63f9c9ab59d7774bf1e2990a2
unyxium <unyxium@gmail.com>
2022-06-23 16:58:16 +1000

    icon location check

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 #from rhino3dm import *
 import ghhops_server as hs
+import os
 import time
 import scraper
 import re
@@ -8,16 +9,21 @@ import re
 app = Flask(__name__)
 hops = hs.Hops(app)
 
+if os.getenv('HOME') == '/home/YCbCr':
+    iconloc = '/home/YCbCr/hops/icons/'
+else:
+    iconloc = 'icons/'
+
 
 @hops.component(
-    "/info",
-    name="Info",
-    description="Basic info",
-    icon="icons/star.png",
+    '/info',
+    name='Info',
+    description='Basic info',
+    icon=iconloc+'star.png',
     inputs=[],
     outputs=[
-        hs.HopsInteger("Time", "T", "Current time for version tracking"),
-        hs.HopsString("Author", "A", "Author")
+        hs.HopsInteger('Time', 'T', 'Current time for version tracking'),
+        hs.HopsString('Author', 'A', 'Author')
     ]
 )
 def info():
@@ -25,15 +31,15 @@ def info():
 
 
 @hops.component(
-    "/getwebpage",
-    name="Get Webpage",
-    description="Download a webpage",
-    icon="icons/star.png",
+    '/getwebpage',
+    name='Get Webpage',
+    description='Download a webpage',
+    icon=iconloc+'star.png',
     inputs=[
-        hs.HopsString("URL", "U", "Webpage URL"),
+        hs.HopsString('URL', 'U', 'Webpage URL'),
     ],
     outputs=[
-        hs.HopsString("Page", "P", "Webpage")
+        hs.HopsString('Page', 'P', 'Webpage')
     ]
 )
 def ghgwp(location):
@@ -41,20 +47,20 @@ def ghgwp(location):
 
 
 @hops.component(
-    "/scrapedoc",
-    name="Scrape Document",
-    description="Scrape a document for data",
-    icon="icons/star.png",
+    '/scrapedoc',
+    name='Scrape Document',
+    description='Scrape a document for data',
+    icon=iconloc+'star.png',
     inputs=[
-        hs.HopsString("Document", "D", "Document at a single object"),
-        hs.HopsString("Elements", "E", "List of targeted elements in order",
-                      access="HopsParamAccess.TREE"),
-        hs.HopsString("Attributes", "A", "List of attributes to filter elements",
-                      access="HopsParamAccess.TREE",
+        hs.HopsString('Document', 'D', 'Document at a single object'),
+        hs.HopsString('Elements', 'E', 'List of targeted elements in order',
+                      access='HopsParamAccess.TREE'),
+        hs.HopsString('Attributes', 'A', 'List of attributes to filter elements',
+                      access='HopsParamAccess.TREE',
                       optional=True, default=None)
     ],
     outputs=[
-        hs.HopsString("Output", "O", "Output data")
+        hs.HopsString('Output', 'O', 'Output data')
     ]
 )
 def ghscrapedoc(document, tree, attributes):
@@ -68,15 +74,15 @@ def ghscrapedoc(document, tree, attributes):
 
 
 @hops.component(
-    "/striptags",
-    name="Strip Tags",
-    description="Strip tags from input and return plain text",
-    icon="icons/star.png",
+    '/striptags',
+    name='Strip Tags',
+    description='Strip tags from input and return plain text',
+    icon=iconloc+'star.png',
     inputs=[
-        hs.HopsString("Input", "I", "Input text")
+        hs.HopsString('Input', 'I', 'Input text')
     ],
     outputs=[
-        hs.HopsString("Output", "O", "Output text")
+        hs.HopsString('Output', 'O', 'Output text')
     ]
 )
 def striptags(document):
@@ -84,15 +90,15 @@ def striptags(document):
 
 
 @hops.component(
-    "/test",
-    name="TEST",
-    description="Testing",
-    icon="icons/star.png",
+    '/test',
+    name='TEST',
+    description='Testing',
+    icon=iconloc+'star.png',
     inputs=[
-        hs.HopsString("Input", "I", "Input values")
+        hs.HopsString('Input', 'I', 'Input values')
     ],
     outputs=[
-        hs.HopsString("Output", "O", "Output values")
+        hs.HopsString('Output', 'O', 'Output values')
     ]
 )
 def testing(input):
